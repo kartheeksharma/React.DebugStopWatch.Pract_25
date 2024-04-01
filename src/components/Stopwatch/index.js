3f8ec0066638259eda35015f72336c3f79d1b018
kartheek03 <kartheeksharma03@gmail.com>
2024-04-01 15:19:53 +0000

    debug stop watch

---
+++ src/components/Stopwatch/index.js
@@ -0,0 +1,112 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Stopwatch extends Component {
+  state = {
+    isTimerRunning: false,
+    timeElapsedInSeconds: 0,
+  }
+  //Fix6: spelling of this lifecycle method should be componentWillUnmount
+  componentWillUnmount() {
+    //Fix7: clearInterval method should be used
+    clearInterval(this.timeInterval)
+  }
+
+  onResetTimer = () => {
+    // Fix4: class variables should be accessed using "this" keyword
+    clearInterval(this.timeInterval)
+    // Fix5: The timer should be reset to 0 when the "Reset" button is clicked
+    this.setState({isTimerRunning: false, timeElapsedInSeconds: 0})
+  }
+
+  onStopTimer = () => {
+    clearInterval(this.timeInterval)
+    this.setState({isTimerRunning: false})
+  }
+
+  updateTime = () => {
+    this.setState(prevState => ({
+      // Fix1: The key timeElapsedInSeconds in prevState should be used to update the state
+      timeElapsedInSeconds: prevState.timeElapsedInSeconds + 1,
+    }))
+  }
+
+  onStartTimer = () => {
+    // Fix2: setInterval method should be used
+    // Fix3: The id returned by setInterval should be stored in class variable to access it across other methods in the class using "this" keyword
+    this.timeInterval = setInterval(this.updateTime, 1000)
+    this.setState({isTimerRunning: true})
+  }
+
+  renderSeconds = () => {
+    const {timeElapsedInSeconds} = this.state
+    const seconds = Math.floor(timeElapsedInSeconds % 60)
+
+    if (seconds < 10) {
+      return `0${seconds}`
+    }
+    return seconds
+  }
+
+  renderMinutes = () => {
+    const {timeElapsedInSeconds} = this.state
+    const minutes = Math.floor(timeElapsedInSeconds / 60)
+
+    if (minutes < 10) {
+      return `0${minutes}`
+    }
+    return minutes
+  }
+
+  render() {
+    const {isTimerRunning} = this.state
+    // Fix8: class methods should be accessed using "this" keyword
+    const time = `${this.renderMinutes()}:${this.renderSeconds()}`
+
+    return (
+      <div className="app-container">
+        <div className="stopwatch-container">
+          <h1 className="stopwatch">Stopwatch</h1>
+          <div className="timer-container">
+            <div className="timer">
+              <img
+                className="timer-image"
+                src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png"
+                alt="stopwatch"
+              />
+              <p className="timer-text">Timer</p>
+            </div>
+            <h1 className="stopwatch-timer">{time}</h1>
+            <div className="timer-buttons">
+              <button
+                type="button"
+                className="start-button button"
+                onClick={this.onStartTimer}
+                disabled={isTimerRunning}
+              >
+                Start
+              </button>
+              <button
+                type="button"
+                className="stop-button button"
+                onClick={this.onStopTimer}
+              >
+                Stop
+              </button>
+              <button
+                type="button"
+                className="reset-button button"
+                onClick={this.onResetTimer}
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Stopwatch
